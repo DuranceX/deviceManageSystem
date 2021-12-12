f02b8af30f5f8cb9d44f5ffa7f772959b90eea7b
Cardy Tse <1124804361@qq.com>
2021-12-12 09:39:15 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,11 @@ module.exports={
                 target:"http://8.130.11.252:8080",
                 //但这种情况会导致转发给目标服务器时仍然带着这个前缀名，而目标服务器上没有这个前缀名路径
                 //所以会导致请求出错，因此这里通过pathRewrite来对路径进行一个正则化处理，去掉前缀名
-                pathRewrite:{'^/server':''} //匹配所有带前缀名的字符串，将前缀名替换成空字符串
+                pathRewrite:{'^/server':''}, //匹配所有带前缀名的字符串，将前缀名替换成空字符串
                 //ws:true        //用于支持websocket
-                //若为true则是如实提供请求地址（即本机），若为false则向目标服务器提供一个假的请求地址
-                //例如此处，如果为真，则为localhost:8080，若为假，则为"目标服务器“
-                //changeOrigin:false     
+                changeOrigin:true,
             }
         },
     },
     publicPath:'/deviceManageSystem/',
-}
+} 
